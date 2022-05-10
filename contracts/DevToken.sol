fd79a5a12d95d807274639073ae797e46fd4c18e
Marco Medrano <marco.medrano.garay@gmail.com>
2022-05-10 00:03:23 -0400

    code to setup balance

--- contracts/DevToken.sol
+++ contracts/DevToken.sol
@@ -1,27 +1,53 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.4;
 
-// contract Migrations {
-//   address public owner = msg.sender;
-//   uint public last_completed_migration;
-
-//   modifier restricted() {
-//     require(
-//       msg.sender == owner,
-//       "This function is restricted to the contract's owner"
-//     );
-//     _;
-//   }
-
-//   function setCompleted(uint completed) public restricted {
-//     last_completed_migration = completed;
-//   }
-// }
-
+/**
+* @notice DevToken is a development token that we use to learn how to code solidity 
+* and what BEP-20 interface requires
+*/
 contract DevToken {
   
-  // Constructor is a function that will run when the Token is created.
-  constructor() {
 
+  /**
+  * @notice Our Tokens required variables that are needed to operate everything
+  */
+  uint private _totalSupply;
+  uint8 private _decimals;
+  string private _symbol;
+  string private _name;
+
+  /**
+  * @notice _balances is a mapping that contains a address as KEY 
+  * and the balance of the address as the value
+  */
+  mapping (address => uint256) private _balances;
+
+  /**
+  * @notice Events are created below.
+  * Transfer event is a event that notify the blockchain that a transfer of assets has taken place
+  *
+  */
+  event Transfer(address indexed from, address indexed to, uint256 value);
+
+  /**
+  * @notice constructor will be triggered when we create the Smart contract
+  * _name = name of the token
+  * _short_symbol = Short Symbol name for the token
+  * token_decimals = The decimal precision of the Token, defaults 18
+  * _totalSupply is how much Tokens there are totally 
+  */
+  constructor(string memory token_name, string memory short_symbol, uint8 token_decimals, uint256 token_totalSupply){
+      _name = token_name;
+      _symbol = short_symbol;
+      _decimals = token_decimals;
+      _totalSupply = token_totalSupply;
+
+      // Add all the tokens created to the creator of the token
+      _balances[msg.sender] = _totalSupply;
+
+      // Emit an Transfer event to notify the blockchain that an Transfer has occured
+      emit Transfer(address(0), msg.sender, _totalSupply);
   }
+  
+
 }
